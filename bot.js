bcb3e823167871b452301e1040dccd921ae75bb9
Zianii <41494410+Zianii@users.noreply.github.com>
2018-10-09 11:51:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'x'
+const prefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`Start ${new Date()}`);
+  console.log(`[Start] ${new Date()}`);
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
@@ -25,14 +25,13 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-	
 });
 
 
 
 
 
-const adminprefix = "x";
+const adminprefix = "*";
 const devs = ['283580465862934539','283580465862934539'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -48,21 +47,6 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 });
 
-const adminprefix = "X";
-const devs = ['283580465862934539','283580465862934539'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else   
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/xmas");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-});
 
 
 
